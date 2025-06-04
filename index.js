cf89b606f81b0a96dca82eef4a26e0579e2b0b5b
annet <annetgnyabuto@gmail.com>
2025-06-04 16:00:54 +0300

    Working with conditional statements

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let scuber
+  if (ride <= 400) {
+    scuber = 'This one is on me!';
+  } else if (ride>2500) {
+    scuber = 'No can do.';
+  } else if (ride > 2000){
+    scuber = 'I will gladly take your thirty bucks.';
+  } else if (ride > 400 && ride < 2000){
+    scuber = "That will be twenty bucks.";
+  }
+  return scuber;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
-}
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+} 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  // Write your code here
+  switch(tip) {
+    case 'generous':
+      return'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
 }
